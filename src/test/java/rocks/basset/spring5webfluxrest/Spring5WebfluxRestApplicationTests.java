ca5ccdf0bc83197d1cefe2c027d793eb377db19a
Thomas Basset <thomas@basset.rocks>
2023-03-03 08:57:40 +0100

    Initial commit

---
+++ src/test/java/rocks/basset/spring5webfluxrest/Spring5WebfluxRestApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.basset.spring5webfluxrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebfluxRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
